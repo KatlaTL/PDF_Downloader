33268a67099fa3352db7e38c470c1a57c2ee52f8
Asger <regsa13@gmail.com>
2026-02-26 20:28:18 +0100

    changed excel column mapping from enum custom attributes to appsettings to make it robust and easier to maintain

--- Infrastructure/Excel/ClosedXMLReportSource.cs
+++ Infrastructure/Excel/ClosedXMLReportSource.cs
@@ -1,17 +1,23 @@
 using Downloader.shared;
 using ClosedXML.Excel;
+using Microsoft.Extensions.Options;
 
 public class ClosedXMLReportSource : IReportSource
 {
+    private readonly ExcelColumnSettings _columns;
+    public ClosedXMLReportSource(IOptions<AppSettings> _settings)
+    {
+        _columns = _settings.Value.ExcelColumns;
+    }
     public IReadOnlyCollection<Report> ReadLinks(string path, int? rows)
     {
         using var wb = new XLWorkbook(path);
 
         var ws = wb.Worksheet(1);
 
-        int fileNameCol = ExcelColumns.FileName.ColumnNumber();
-        int pdfUrlCol = ExcelColumns.PdfUrl.ColumnNumber();
-        int htmlUrlCol = ExcelColumns.ReportHtmlUrl.ColumnNumber();
+        int fileNameCol = ExcelColumnConverter.LetterToNumber(_columns.FileName);
+        int pdfUrlCol = ExcelColumnConverter.LetterToNumber(_columns.PdfUrl);
+        int htmlUrlCol = ExcelColumnConverter.LetterToNumber(_columns.ReportHtmlUrl);
 
         var lastRow = rows ?? ws.LastRowUsed()?.RowNumber() ?? 0;
         var links = new List<Report>(lastRow);
